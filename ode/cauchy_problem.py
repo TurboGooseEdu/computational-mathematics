67598b4dc3bcbcb31c95b76747dc73f5cc1e03f3
Ilya <antonpuckich@gmail.com>
2021-06-16 20:58:46 +0300

    The first task and two subtasks of the second task implemented

--- ode/cauchy_problem.py
+++ ode/cauchy_problem.py
@@ -9,7 +9,6 @@ x0 = 0.
 Y0 = np.array([1., 1., A, 1.])
 x_start = 0
 x_fin = 5
-tol = 1.e-5
 
 
 def generate_RKM2(c):
@@ -20,20 +19,13 @@ considered_method = generate_RKM2(c2)
 opponent_method = generate_RKM2(1/2)  # midpoint
 
 
-def y1(x):
-    return np.exp(np.sin(x**2))
-
-
-def y2(x):
-    return np.exp(B * np.sin(x**2))
-
-
-def y3(x):
-    return C * np.sin(x**2) + A
-
-
-def y4(x):
-    return np.cos(x**2)
+def exact_solution(x):
+    return np.array([
+        np.exp(np.sin(x ** 2)),
+        np.exp(B * np.sin(x ** 2)),
+        C * np.sin(x ** 2) + A,
+        np.cos(x ** 2)
+    ])
 
 
 def F(x, y):
@@ -45,20 +37,51 @@ def F(x, y):
     ])
 
 
-def solve_constant_step(method, h):
-    N = int((x_fin - x_start) / h)
-    x = x0
+def solve_constant_step(start, stop, h, method):
     Y = Y0
-    x_values = [x]
+    x_values = []
     Y_values = [Y]
-    for i in range(N):
+    for x in np.arange(start, stop, h):
         Y = method.calculate(F, h, x, Y)
-        print("{}) {}".format(i + 1, Y))
-        x += h
         x_values.append(x)
         Y_values.append(Y)
-    if x < x_fin:
-        x_values.append(x_fin)
-        Y_values.append(method.calculate(F, x_fin - x, x, Y))
-
+    if x_values[-1] < stop:
+        x_values.append(stop)
+    else:
+        del Y_values[-1]
     return x_values, Y_values
+
+
+def eval_error_by_Runge(Yn, Y2n, method):
+    p = method.precision
+    R2n = (Y2n - Yn) / (2**p - 1)
+    return R2n
+
+
+def solve_auto_step(start, stop, method):
+    h = 0.02
+    tol = 1.e-6
+    factor = 0.9
+    facmin = 0.3
+    facmax = 3
+    p = method.precision
+    Y_cur = Y0
+    x_cur = start
+    x_values = [x_cur]
+    x_discarded = []
+    Y_values = [Y_cur]
+    while x_cur < stop:
+        Yn = method.calculate(F, h, x_cur, Y_cur)
+        Y_mid = method.calculate(F, h/2, x_cur, Y_cur)
+        Y2n = method.calculate(F, h/2, x_cur + h/2, Y_mid)
+        R2n = eval_error_by_Runge(Yn, Y2n, method)
+        R2n_norm = np.linalg.norm(R2n)
+        if R2n_norm < tol:
+            Y_cur = Y2n + R2n
+            x_cur += h
+            x_values.append(x_cur)
+            Y_values.append(Y_cur)
+        elif len(x_discarded) == 0 or x_cur != x_discarded[-1]:
+            x_discarded.append(x_cur)
+        h = min(stop - x_cur, h * max(facmin, min(facmax, factor * (tol / R2n_norm)**(1/p))))
+    return x_values, Y_values, x_discarded
--- ode/tests.py
+++ ode/tests.py
@@ -2,33 +2,124 @@ from ode.cauchy_problem import *
 import matplotlib.pyplot as plt
 
 
-def render_plot_with_numerical_and_exact_solutions(solution):
-    x_values, Y = solution
-    n = len(Y[0])
+def render_plot_with_numerical_and_exact_solutions(x_values, Y_values):
+    n = len(Y_values[0])
     y_values_num = [[] for _ in range(n)]
-    for y in Y:
+    for y in Y_values:
         for i in range(n):
             y_values_num[i].append(y[i])
 
     y_values_exact = [[] for _ in range(n)]
-    exact_solutions = [y1, y2, y3, y4]
-    for i in range(len(x_values)):
+    for x in x_values:
+        exact_sol = exact_solution(x)
         for j in range(n):
-            y_values_exact[j].append(exact_solutions[j](x_values[i]))
+            y_values_exact[j].append(exact_sol[j])
 
-    colors = ["r", "g", "b", "y"]
-    for i in range(n):
-        plt.plot(x_values, y_values_exact[i], colors[i % n])
-        plt.plot(x_values, y_values_num[i], colors[i] + "--")
+    plt.title("Integral curves")
+    plt.plot(x_values, y_values_exact[0], "r")
+    plt.plot(x_values, y_values_num[0], "r--")
+    plt.plot(x_values, y_values_exact[1], "g")
+    plt.plot(x_values, y_values_num[1], "g--")
+    plt.plot(x_values, y_values_exact[2], "b")
+    plt.plot(x_values, y_values_num[2], "b--")
+    plt.plot(x_values, y_values_exact[3], "y")
+    plt.plot(x_values, y_values_num[3], "y--")
+    plt.legend(["exact solution", "numerical solution"])
     plt.grid()
     plt.show()
 
 
-def test():
-    res = solve_constant_step(considered_method, 0.005)
-    render_plot_with_numerical_and_exact_solutions(res)
+def test0():
+    render_plot_with_numerical_and_exact_solutions(solve_constant_step(x_start, x_fin, 0.01, considered_method))
+
+
+def test1():
+    h = 0.01
+    h_values = []
+    errors = []
+    Yn_exact = exact_solution(x_fin)
+    for i in range(5):
+        h_values.append(h)
+        errors.append(np.linalg.norm(solve_constant_step(x_start, x_fin, h, considered_method)[1][-1] - Yn_exact))
+        h /= 2
+
+    plt.title("error(h)")
+    plt.plot(h_values, errors)
+    plt.semilogy(base=2)
+    plt.semilogx(base=2)
+    plt.xlabel("h")
+    plt.ylabel("error")
+    plt.grid()
+    plt.show()
+
+
+def test2():
+    h = 0.01
+    tol = 1.e-5
+    p = considered_method.precision
+    Yn = solve_constant_step(x_start, x_fin, h, considered_method)[1][-1]
+    Y2n = solve_constant_step(x_start, x_fin, h/2, considered_method)[1][-1]
+    R2n = eval_error_by_Runge(Yn, Y2n, considered_method)
+    h_opt = h / 2 * (tol / np.linalg.norm(R2n))**(1 / p)
+    x_values, Y_values = solve_constant_step(x_start, x_fin, h_opt, considered_method)
+    # render_plot_with_numerical_and_exact_solutions(x_values, Y_values)
+    errors = []
+    for i in range(len(x_values)):
+        errors.append(np.linalg.norm(Y_values[i] - exact_solution(x_values[i])))
+
+    # Yn_exact = exact_solution(x_fin)
+    # for Y in Y_values:
+    #     errors.append(np.linalg.norm(Y - Yn_exact))
+
+    plt.title("error(x)")
+    plt.plot(x_values, errors)
+    plt.xlabel("x")
+    plt.ylabel("error")
+    plt.grid()
+    plt.show()
+
+
+def render_plot_for_auto_step(x_values, x_discarded):
+    i_d = 0
+    i_x = 0
+    h_values = []
+    h_discarded = []
+    x_accepted = []
+    h_accepted = []
+
+    while i_d < len(x_discarded) and i_x < len(x_values) - 1:
+        h = x_values[i_x + 1] - x_values[i_x]
+        h_values.append(h)
+        if x_values[i_x] == x_discarded[i_d]:
+            h_discarded.append(h)
+            i_d += 1
+        else:
+            h_accepted.append(h)
+            x_accepted.append(x_values[i_x])
+        i_x += 1
+
+    while i_x < len(x_values) - 1:
+        h = x_values[i_x + 1] - x_values[i_x]
+        h_values.append(h)
+        h_accepted.append(h)
+        x_accepted.append(x_values[i_x])
+        i_x += 1
+
+    plt.plot(x_values[:-1], h_values)
+    plt.plot(x_accepted, h_accepted, "ro")
+    plt.plot(x_discarded, h_discarded, "bx")
+    plt.title("h(x)")
+    plt.xlabel("x")
+    plt.ylabel("h")
+    plt.grid()
+    plt.show()
+
+
+def test3():
+    x_values, Y_values, x_discarded = solve_auto_step(x_start, x_fin, considered_method)
+    render_plot_for_auto_step(x_values, x_discarded)
 
 
 if __name__ == '__main__':
-    test()
+    test3()
 
